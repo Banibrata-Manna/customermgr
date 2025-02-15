04834e8b802adab394624b2ada3e73fc8d596160
Banibrata-Manna <mbanibrata668@gmail.com>
2025-02-15 23:57:25 +0530

    Progress on find

--- groovyScripts/crud/FindCustomerViewService.groovy
+++ groovyScripts/crud/FindCustomerViewService.groovy
@@ -2,7 +2,9 @@ import org.apache.ofbiz.base.*;
 import org.apache.ofbiz.entity.*;
 import org.apache.ofbiz.service.*;
 
-
+customerList = delegator.findList("FindCustomerView", null, null , null, null, false);
+context.customerList = customerList;
+//context.put("customerList", customerList);
 
 //ofbizDemoTypes = delegator.findList("OfbizDemoType", null, null, null, null, false);
 //context.ofbizDemoTypes = ofbizDemoTypes;
