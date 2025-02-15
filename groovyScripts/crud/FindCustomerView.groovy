63068af488eea6b4ef3da436a1fd2f8e1eba2ac2
Banibrata-Manna <mbanibrata668@gmail.com>
2025-02-15 11:18:25 +0530

    Made directories for groovy scripts and ftl files and introduced new member entity PartyContactMechPurpose in the FindCustomerView entity

---
+++ groovyScripts/crud/FindCustomerView.groovy
@@ -0,0 +1,10 @@
+import org.apache.ofbiz.base.*;
+import org.apache.ofbiz.entity.*;
+import org.apache.ofbiz.service.*;
+
+
+
+//ofbizDemoTypes = delegator.findList("OfbizDemoType", null, null, null, null, false);
+//context.ofbizDemoTypes = ofbizDemoTypes;
+//ofbizDemoList = delegator.findList("OfbizDemo", null, null, null, null, false);
+//context.ofbizDemoList = ofbizDemoList;
