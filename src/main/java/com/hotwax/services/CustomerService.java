c05da88c96d176442481993c3dee10577fe7750e
Banibrata-Manna <mbanibrata668@gmail.com>
2025-02-16 09:38:41 +0530

    Fixed View entity
    Modified create service definition
    
    Modified CustomerService java service

--- src/main/java/com/hotwax/services/CustomerService.java
+++ src/main/java/com/hotwax/services/CustomerService.java
@@ -81,7 +81,6 @@ public class CustomerService{
       Debug.logError(e, MODULE);
       return ServiceUtil.returnError("Failed to create record " + MODULE);
     }
-    Debug.logInfo("///////////////////////////${result.customersList}////////////////////////////", MODULE);
     return result;
   }
 
@@ -89,8 +88,17 @@ public class CustomerService{
     public static Map<String, Object> createCustomer(DispatchContext dispatchContext, Map<String, ? extends Object> context) {
     Map<String, Object> result = ServiceUtil.returnSuccess();
     Delegator delegator = dispatchContext.getDelegator();
+    LocalDispatcher dispatcher = dispatchContext.getDispatcher();
     Timestamp now = Timestamp.valueOf(LocalDateTime.now());
     try{
+      Map<String, Object> ifExists = dispatcher.runSync("findCustomers",
+          UtilMisc.toMap("emailAddress",
+              context.get("emailAddress")));
+
+      List<GenericValue> existingParty = (List)ifExists.get("customersList");
+
+      if(!existingParty.isEmpty()) return result;
+
       GenericValue party = delegator.makeValue("Party");
       party.setNextSeqId();
       party.setFields(UtilMisc.toMap("partyTypeId", "PERSON"));
@@ -108,12 +116,13 @@ public class CustomerService{
 
       GenericValue contactMech = delegator.makeValue("ContactMech");
       contactMech.setNextSeqId();
-      contactMech.setNonPKFields(UtilMisc.toMap("infoString", context.get("emailAddress")));
+      contactMech.setNonPKFields(UtilMisc.toMap("infoString", context.get("emailAddress"),
+          "contactMechTypeId", "EMAIL_ADDRESS"));
       contactMech = delegator.create(contactMech);
 
       GenericValue partyContactMech = delegator.makeValue("PartyContactMech");
       partyContactMech.setPKFields(UtilMisc.toMap("partyId", party.get("partyId"),
-        "contactMechId", contactMech.get("contactMechId"), "fromDate", now,
+          "contactMechId", contactMech.get("contactMechId"), "fromDate", now,
           "roleTypeId", "CUSTOMER"));
       partyContactMech = delegator.create(partyContactMech);
 
@@ -122,6 +131,67 @@ public class CustomerService{
           "contactMechId", contactMech.get("contactMechId"), "fromDate", now,
           "contactMechPurposeTypeId", "PRIMARY_EMAIL"));
       partyContactMechPurpose = delegator.create(partyContactMechPurpose);
+
+      if(!UtilValidate.isEmpty(context.get("postalCode"))){
+        Debug.logInfo((String) context.get("postalCode"), MODULE);
+        Debug.logInfo("///////////////////////// Reached postal ////////////////////////", MODULE);
+        contactMech = delegator.makeValue("ContactMech");
+        contactMech.setNextSeqId();
+        contactMech.setNonPKFields(UtilMisc.toMap("contactMechTypeId", "POSTAL_ADDRESS"));
+        contactMech = delegator.create(contactMech);
+
+        GenericValue postalAddress = delegator.makeValue("PostalAddress");
+        postalAddress.setPKFields(UtilMisc.toMap("contactMechId",
+            contactMech.get("contactMechId")));
+        postalAddress.setNonPKFields(context);
+
+        postalAddress = delegator.create(postalAddress);
+
+        Debug.logInfo(postalAddress.toString(), MODULE);
+
+        partyContactMech = delegator.makeValue("PartyContactMech");
+        partyContactMech.setPKFields(UtilMisc.toMap("partyId", party.get("partyId"),
+            "contactMechId", contactMech.get("contactMechId"), "fromDate", now,
+            "roleTypeId", "CUSTOMER"));
+        partyContactMech = delegator.create(partyContactMech);
+
+        partyContactMechPurpose = delegator.makeValue("PartyContactMechPurpose");
+        partyContactMechPurpose.setPKFields(UtilMisc.toMap("partyId", party.get("partyId"),
+            "contactMechId", contactMech.get("contactMechId"), "fromDate", now,
+            "contactMechPurposeTypeId", "PRIMARY_LOCATION"));
+        partyContactMechPurpose = delegator.create(partyContactMechPurpose);
+      }
+
+      if(!UtilValidate.isEmpty(context.get("contactNumber"))){
+        Debug.logInfo((String) context.get("contactNumber"), MODULE);
+        Debug.logInfo("///////////////////////// Reached Telecom ////////////////////////", MODULE);
+
+        contactMech = delegator.makeValue("ContactMech");
+        contactMech.setNextSeqId();
+        contactMech.setNonPKFields(UtilMisc.toMap("contactMechTypeId", "TELECOM_NUMBER"));
+        contactMech = delegator.create(contactMech);
+
+        GenericValue telecomNumber = delegator.makeValue("TelecomNumber");
+        telecomNumber.setPKFields(UtilMisc.toMap("contactMechId",
+            contactMech.get("contactMechId")));
+        telecomNumber.setNonPKFields(context);
+
+        telecomNumber = delegator.create(telecomNumber);
+
+        Debug.logInfo(telecomNumber.toString(), MODULE);
+
+        partyContactMech = delegator.makeValue("PartyContactMech");
+        partyContactMech.setPKFields(UtilMisc.toMap("partyId", party.get("partyId"),
+            "contactMechId", contactMech.get("contactMechId"), "fromDate", now,
+            "roleTypeId", "CUSTOMER"));
+        partyContactMech = delegator.create(partyContactMech);
+
+        partyContactMechPurpose = delegator.makeValue("PartyContactMechPurpose");
+        partyContactMechPurpose.setPKFields(UtilMisc.toMap("partyId", party.get("partyId"),
+            "contactMechId", contactMech.get("contactMechId"), "fromDate", now,
+            "contactMechPurposeTypeId", "PRIMARY_PHONE"));
+        partyContactMechPurpose = delegator.create(partyContactMechPurpose);
+      }
     } catch (Exception e) {
       Debug.logError(e, MODULE);
       return ServiceUtil.returnError("Failed to create record " + MODULE);
